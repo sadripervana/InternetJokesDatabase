82883d9cd51f68cd97cac524b8df78ff44898ca4
sadripervana <sadripervana@gmail.com>
2021-10-06 13:32:23 +0200

    modified form

--- templates/login.html.php
+++ templates/login.html.php
@@ -1,13 +1,27 @@
 <?php 
-	if(isset($error)): 
-		echo '<div class="errors">' . $error . '</div>';
+if(isset($error)): 
+	echo '<div class="errors">' . $error . '</div>';
 
-	endif; ?>
-<form method="post" action="">
-	<label for="email">Your email address</label>
-	<input type="text" id="email" name="email">
-	<label for="password">Your password</label>
-	<input type="password" id="password" name="password">
-	<input type="submit" name="login" value="Log in">
+endif; ?>
+<form class="form-style" method="post" action="">
+	<div class="form-group row">
+		<div class="form-group row">
+			<label for="email" >Your email address</label>
+			<div class="col-sm-10">
+				<input type="text" id="email" name="email">
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="password"  >Your password</label>
+			<div class="col-sm-10">
+				<input type="password" id="password" name="password">
+			</div>
+		</div>
+	</div>
+	<div class="form-group row">
+		<div class="col-sm-10 add">
+			<button type="submit" name="login" class="btn btn-primary ">Login</button>
+		</div>
+	</div>
 </form>
 <p>Don't have an account? <a href="/author/register">Click here to register an account</a></p>
