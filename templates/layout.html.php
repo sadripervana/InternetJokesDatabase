d604f461683ff8c78162ab77b36b16c5739527a6
sadripervana <sadripervana@gmail.com>
2021-10-06 13:31:53 +0200

    styled navbar and added bootstrap link

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -3,27 +3,29 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="/joke.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <title><?=$title?></title>
   </head>
   <body>
     <header>
-      <h1>Internet Joke Database</h1>
-    </header>
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/joke/list">Jokes List </a></li>
-        <li><a href="/joke/edit">Add a new Joke</a></li>
-        <li><a href="/author/register">Register Autor</a></li>
+      <h4>Internet Joke Database</h4>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <ul class="navbar-nav mr-auto ">
+        <li class="nav-item active"><a class="nav-link" href="/">Home</a></li>
+        <li class="nav-item "><a class="nav-link" href="/joke/list">Jokes List </a></li>
+        <li class="nav-item "><a class="nav-link" href="/joke/edit">Add a new Joke</a></li>
+        <li class="nav-item "><a class="nav-link" href="/author/register">Register Autor</a></li>
         <?php if ($loggedIn): ?>
-            <li><a href="/logout">Log out</a></li>
+            <li class="nav-item "><a class="nav-link" href="/logout">Log out</a></li>
           <?php else: ?>
-            <li><a href="/login">Log in</a></li>
+            <li class="nav-item "><a class="nav-link" href="/login">Log in</a></li>
         <?php endif; ?>
-        <li><a href="/category/list">Category List</a></li>
-        <li><a href="/author/list">Author List</a></li>
+        <li class="nav-item "><a class="nav-link" href="/category/list">Category List</a></li>
+        <li class="nav-item "><a class="nav-link" href="/author/list">Author List</a></li>
       </ul>
       </nav>
+      </header>
+
       <main>
         <?=$output?>
       </main>
